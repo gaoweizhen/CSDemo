7328ee5496bdecbac9f321bedc3e3057d46a0ad6
fergus.gao <fergus.gao@utstarcom.cn>
2020-05-11 15:05:06 +0800

    Initial commit

---
+++ app/src/main/java/com/gao/csdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.gao.csdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+*日期:2020/5/11
+ * 时间:15:00
+ * 作者:gaoweizhen
+*类描述：测试分支管理
+*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
