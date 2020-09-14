d857369ca15d577ec4ff16e563d28f41908575a1
fergus.gao <fergus.gao@utstarcom.cn>
2020-09-14 18:26:23 +0800

    分支测试1111222

--- app/src/main/java/com/gao/csdemo/MainActivity.java
+++ app/src/main/java/com/gao/csdemo/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//数据未同步信息
     }
 }
